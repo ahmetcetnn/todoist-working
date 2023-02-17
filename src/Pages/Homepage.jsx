70d265f9dbed3d1f32eef19b830a879cfc4a1733
ahmetcetnn <ahmetlaw@outlook.com>
2023-02-17 23:48:29 +0300

    completed parttwo

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Todoist from './Todoist'
 import { Link } from 'react-router-dom'
+import homepageparttwo from '../images/homepageparttwo.avif'
+import homepageparttwoleft from "../images/homepageparttwoleft.webp"
+import homepageparttworight from "../images/homepageparttworight.webp"
+
 function Homepage() {
   return (
     <div className='homegeneral'>
@@ -11,13 +15,34 @@ function Homepage() {
                     <li>Şablonlar</li>
                     <li>Ekipler İçin</li>
                     <li>Ücretlendirme</li>
-                    
-                    <li>Giriş Yap</li>
-                    <li>Ücretsiz Başla</li>
+                    <li>|</li>
+                    <li><Link to="/login">Giriş Yap</Link></li>
+                    <li><Link to ="/register">Ücretsiz Başla</Link></li>
                 </div>
-
             </div>
-      
+        
+        
+        
+        
+            <div className='homepage__partone'>
+                <h1>iş ve özel hayatını düzenle.</h1>
+                <h3>Todoist ile daha odaklı,düzenli ve sakin kal.</h3>
+                <h3>Dünyanın #1 numaralı görev yönetimi ve</h3>
+                <h3>yapılacaklar-listesi uygulaması.</h3>
+                <button id='homepagebtn'>Ücretsiz Başla </button>
+            </div>
+
+
+
+            <div className='homepage__parttwo'>
+                <img src={homepageparttwo} alt="hpptwo" id='hpptwo'/>
+                <img src={homepageparttwoleft} alt="hpptwoleft" id='hpptwoleft'/>
+                {/* <img src={homepageparttworight} alt="hpptworight" id='hpptworight'/> */}
+            </div>
+
+            <div className='homepage__partthree'>
+                
+            </div>
     </div>
   )
 }
