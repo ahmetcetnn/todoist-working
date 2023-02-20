c4fbe94669af569fd6886b0f9a58e373bad286d6
ahmetcetnn <ahmetlaw@outlook.com>
2023-02-20 23:25:37 +0300

    few changes

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -13,7 +13,7 @@ function Homepage() {
       <div className="navbar">
        <Link to="/"><Todoist /></Link> 
         <div className="navitems">
-          <li>Özeldlikler</li>
+          <li>Özellikler</li>
           <li>Şablonlsar</li>
           <li>Ekipler İçin</li>
           <li>Ücretlendirme</li>
